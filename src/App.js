6c170995ead343ecd2065e8cd190e38366519980
Trang <trang03011990@gmail.com>
2021-10-14 20:26:52 +0700

    add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import HomeComponent from './BaiTapThucHanhLayout/HomeComponent';
+import HomePage from './BaiTapGlasses/HomePage';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+        {/* <HomeComponent/> */}
+    </div>
+  );
+}
+
+export default App;
